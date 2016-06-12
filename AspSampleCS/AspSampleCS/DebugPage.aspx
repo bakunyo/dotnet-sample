d69d8649545d7cf36357e13f8b97be676ab994a8
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-12 20:47:36 +0900

    Add debug page and put bug

---
+++ AspSampleCS/AspSampleCS/DebugPage.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DebugPage.aspx.cs" Inherits="AspSampleCS.DebugPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="CaptionLabel" runat="server"></asp:Label>
+        <br />
+        <asp:TextBox ID="NumberTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="SquareButton" runat="server" OnClick="SquareButton_Click" Text="Square" />
+        <br />
+        <br />
+        <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AspSampleCS/AspSampleCS/DebugPage.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspSampleCS
+{
+    public partial class DebugPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SquareButton_Click(object sender, EventArgs e)
+        {
+            int number, result;
+            number = System.Convert.ToInt32(NumberTextBox.Text);
+            result = Square(number);
+            ResultLabel.Text = NumberTextBox.Text + " squared is " + result.ToString();
+        }
+
+        int Square(int number)
+        {
+            int Square;
+            Square = number + number;
+            return Square;
+        }
+    }
+}
