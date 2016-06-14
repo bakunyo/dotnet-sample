320c6a4a6d2c12799ca35fd0f6583f73e26104e5
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-14 22:26:48 +0900

    Add Select Case Statement

--- ConsoleSampleVB/ConsoleSampleVB/Module1.vb
+++ ConsoleSampleVB/ConsoleSampleVB/Module1.vb
@@ -9,6 +9,17 @@
             msg = "False"
         End If
         Console.WriteLine(msg)
+
+        Dim i As Integer = 2
+        Select Case i
+            Case 0
+                Console.WriteLine("0")
+            Case 1
+                Console.WriteLine("1")
+            Case 2
+                Console.WriteLine("2")
+        End Select
+
         Console.ReadLine()
     End Sub
 
