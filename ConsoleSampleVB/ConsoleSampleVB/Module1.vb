ce0fcdaa151f468c2a19b1dbd234ed7a05d1e4b0
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-14 22:09:36 +0900

    Use Dim and comment

--- ConsoleSampleVB/ConsoleSampleVB/Module1.vb
+++ ConsoleSampleVB/ConsoleSampleVB/Module1.vb
@@ -1,7 +1,9 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hello, VB")
+        ' Comment
+        Dim msg As String = "Hello, VB"
+        Console.WriteLine(msg)
         Console.ReadLine()
     End Sub
 
