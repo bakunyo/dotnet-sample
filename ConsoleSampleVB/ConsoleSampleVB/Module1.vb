282090720f5aaa520e03b62d7f7dec044cc7791a
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-06-14 22:15:00 +0900

    Use If Else Statement

--- ConsoleSampleVB/ConsoleSampleVB/Module1.vb
+++ ConsoleSampleVB/ConsoleSampleVB/Module1.vb
@@ -2,7 +2,12 @@
 
     Sub Main()
         ' Comment
-        Dim msg As String = "Hello, VB"
+        Dim msg As String
+        If True Then
+            msg = "True"
+        Else
+            msg = "False"
+        End If
         Console.WriteLine(msg)
         Console.ReadLine()
     End Sub
