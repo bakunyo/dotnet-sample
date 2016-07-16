f19fdc28428c64a56b9fa5d4c4d682a6c5b129eb
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-07-17 01:50:31 +0900

    Use Page.IsPostBack

--- SelfAspNet/SelfAspNet/Hello.aspx.vb
+++ SelfAspNet/SelfAspNet/Hello.aspx.vb
@@ -2,7 +2,9 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If Not Page.IsPostBack Then
+            txtName.Text = "権兵衛"
+        End If
     End Sub
 
     Protected Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
